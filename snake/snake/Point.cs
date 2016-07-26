7cb267608d6002ef3fe19a61a3044082d70f45b5
Klem <mr.tonkonog@gmail.com>
2016-07-26 19:07:29 +0300

    Создали класс Point

---
+++ snake/snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}

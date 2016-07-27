46010dc74e7d933e997f7c6d0a77dc1ec24b2910
Klem <mr.tonkonog@gmail.com>
2016-07-27 11:35:07 +0300

    С моими точками

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,22 +10,27 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(8, 2, '*');
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
+            Point p2 = new Point(7, 10, '?');
             p2.Draw();
+
+            Point p3 = new Point(3, 15, '%');
+            p3.Draw();
 
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
             numList.Add(2);
+            numList.Add(3);
 
             int x = numList[ 0 ];
             int y = numList[ 1 ];
             int z = numList[ 2 ];
+            int u = numList[3];
 
-            foreach(int i in numList)
+            foreach (int i in numList)
             {
                 Console.WriteLine(i);
             }
@@ -35,6 +40,7 @@ namespace snake
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
+            pList.Add(p3);
 
             Console.ReadLine();
      

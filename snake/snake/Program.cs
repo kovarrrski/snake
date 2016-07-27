3b604ea92400059da7418dbf0889002453c93a83
Klem <mr.tonkonog@gmail.com>
2016-07-27 10:59:01 +0300

    Хуй

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,15 +10,33 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1( x );
+            Console.WriteLine("Call Func1. x = " + x);
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-  
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1 = p2. p1.x = " + p1.x + ", p1.y = " + p1.y + "; p2.x = " + p2.x + ", p2.y = " + p2.y);
+
+            p1 = new Point(1, 3, '*');
+            Update(p1);
+            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
 
             Console.ReadLine();
-     
 
         }
     }
